15e02c5f2ec59bd6618b5ba18fcc34cd333f7031
RS_Exau_Kim <92597014+RS-malik-el@users.noreply.github.com>
2022-11-17 16:27:32 +0000

    Add files via upload

---
+++ blue_esp32/blue_esp32.ino
@@ -0,0 +1,42 @@
+/**
+ * 
+ * e-mail : rachelsysteme@gmail.com
+ * gitHub : https://github.com/RS-malik-el
+ * Donation : paypal.me/RachelSysteme
+ *
+ * @AUTHEUR : RACHEL SYSTEME
+ * DATE : 15/11/2022
+ * 
+ * * @Board : esp32
+ * 
+ * Dans ce programme, nous allons apprendre à utiliser le bluetooth
+ * avec l'esp32 pour allumer une lampe via un relais
+ * 
+*/
+
+#include "BluetoothSerial.h"
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+BluetoothSerial bluetooth;
+
+#define PIN_RELAIS 23
+
+void setup() {
+  bluetooth.begin("RS_ESP32"); //Nom du bluetooth
+  pinMode(PIN_RELAIS,OUTPUT); 
+}
+
+void loop() {
+	String cmd;  
+	if (bluetooth.available()) {
+    	cmd = bluetooth.readStringUntil('\n');
+    	if (cmd=="on")
+   			digitalWrite(PIN_RELAIS,HIGH);
+   		if (cmd=="off")
+   			digitalWrite(PIN_RELAIS,LOW);
+  	}
+  	delay(20);
+}
