e9d85009f63da34bcfbab7d179c381ce1dd8243d
Exau_KIM <92597014+RS-malik-el@users.noreply.github.com>
2024-01-28 22:31:36 +0000

    Update esp32_boutonPoussoir.ino

--- esp32_boutonPoussoir/esp32_boutonPoussoir.ino
+++ esp32_boutonPoussoir/esp32_boutonPoussoir.ino
@@ -4,7 +4,7 @@
  * gitHub : https://github.com/RS-malik-el
  * Donation : paypal.me/RachelSysteme
  *
- * @AUTHEUR : RACHEL SYSTEME
+ *@AUTEUR : Exaucé KIMBEMBE / OpenProgramming
  * DATE : 30/10/2022
  * 
  * * @Board : esp32
